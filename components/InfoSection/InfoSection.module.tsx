0c90c4ee356c651521d7cb21ba413fa15c89b74a
Stefan Lier <stefan.lier@netlight.com>
2020-12-28 13:58:59 +0100

    Size IMG directly to reduce impact of FOUC

--- components/InfoSection/InfoSection.module.tsx
+++ components/InfoSection/InfoSection.module.tsx
@@ -1,4 +1,5 @@
 import styles from './InfoSection.module.scss';
+import React from 'react';
 
 type InfoSectionProps = {
     image: string;
@@ -6,7 +7,7 @@ type InfoSectionProps = {
 
 const InfoSection: React.FC<InfoSectionProps> = ({image, children}) => {
     return <div className={styles.infoSection}>
-        <img src={`/${image}`} className={styles.infoSection__image} />
+        <img src={`/${image}`} className={styles.infoSection__image} width={100} height={100} />
         <div className={styles.infoSection__content}>{children}</div>
     </div>
 }
