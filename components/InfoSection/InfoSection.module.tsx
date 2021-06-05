6eb06b50687d02f3a5539d1afce4f0ce7198a0cd
Stefan Lier <stefan.lier@netlight.com>
2021-06-05 13:31:53 +0200

    Enable Alt-Text for section images

--- components/InfoSection/InfoSection.module.tsx
+++ components/InfoSection/InfoSection.module.tsx
@@ -3,11 +3,13 @@ import React from 'react';
 
 type InfoSectionProps = {
     image: string;
+    name: string;
 }
 
-const InfoSection: React.FC<InfoSectionProps> = ({image, children}) => {
+const InfoSection: React.FC<InfoSectionProps> = ({image, name, children}) => {
     return <div className={styles.infoSection}>
-        <img src={`/${image}`} className={styles.infoSection__image} width={100} height={100} />
+        <img src={`/${image}`} className={styles.infoSection__image} width={100} height={100}
+             alt={`Avatar for ${name}-Section`}/>
         <div className={styles.infoSection__content}>{children}</div>
     </div>
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -14,21 +14,21 @@ const Home: React.FC = () => {
                 <title>stefan - coding, lifting, living</title>
             </Head>
             <h1 style={{textAlign: 'center'}}>Moin!</h1>
-            <InfoSection image={'avatar_one.jpg'}>
+            <InfoSection name="Study" image={'avatar_one.jpg'}>
                 Hej, my name is Stefan, I'm a {age} year old from Salzwedel, now living in
                 Hamburg. In between, I have been living in Hanover, studying <i>Applied Informatics</i> at the <i>University
                 of Applied Science and Arts Hannover</i>. During that time, I was working as a research associate at
                 the <a href="http://sw-architecture.inform.hs-hannover.de/joomla/index.php/team">Software
                 Architecture Research Group</a>, researching event-based communication between machines.
             </InfoSection>
-            <InfoSection image={'avatar_two.jpg'}>
+            <InfoSection name="Work" image={'avatar_two.jpg'}>
                 Since 2014 I am working as a Consultant at <a href="https://www.netlight.com/">Netlight
                 Consulting</a>, first in Munich, Germany and now in Hamburg, Germany. My goal is helping our clients
                 to achieve something great in their projects, developing software, managing infrastructure and
                 growing teams. I love my work and the people I work with on a daily basis and am eager to see where
                 this leads me in the future.
             </InfoSection>
-            <InfoSection image={'avatar_three.jpg'}>
+            <InfoSection name="Spare time" image={'avatar_three.jpg'}>
                 Besides work, I love my life in Hamburg, enjoying the city and all it has to offer. I'm doing
                 weightlifting in my spare time, challenging myself to go to the deep end. Listening to music nearly
                 every minute of my day, I love to go to concerts on a regular basis and Hamburg and its alternative
