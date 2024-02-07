0a1e8b26770f162e215d488495a8594384436f7d
Stefan Lier <lier.stefan@gmail.com>
2024-02-07 20:49:50 +0100

    Add favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,7 +10,11 @@ class MyDocument extends Document {
         return (
             <Html lang="en">
                 <Head>
-                    <meta name="description" content="Private Home in the internet for Stefan Lier" />
+                    <meta name="description" content="Private Home in the internet for Stefan Lier"/>
+                    <link rel="shortcut icon" href="/favicon.png"/>
+                    <link rel="icon" type="image/png" href="/favicon.png" sizes="32x32"/>
+                    <link rel="icon" type="image/png" href="/favicon.png" sizes="96x96"/>
+                    <link rel="apple-touch-icon" href="/favicon.png" sizes="180x180"/>
                 </Head>
                 <body>
                 <Main/>
